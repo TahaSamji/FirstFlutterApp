b9be11e427b460be95daae825be26b5260d26c96
MUHAMMAD TAHA SAMJI <25186@iba.edu.pk>
2024-10-01 11:09:14 +0500

    Api hit using http package

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,140 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
 }
 
+class Post {
+  final int id;
+  final String url;
+  final String slug;
+  final String content;
+  final String title;
+  final String image;
+
+  Post(
+      {required this.id,
+      required this.url,
+      required this.slug,
+      required this.content,
+      required this.image,
+      required this.title});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+        id: json['id'],
+        slug: json['slug'],
+        url: json['url'],
+        title: json['title'],
+        content: json['content'],
+        image: json['image']);
+  }
+  Map<String,dynamic> toJson(){
+    return {
+      'id':id,
+      "url" : url,
+      "title" : title,
+      "content" :content,
+      "image" :image
+    };
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const ListUser(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+//   final String title;
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
 
-  final String title;
+class ListUser extends StatefulWidget {
+  const ListUser({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ListUser> createState() => _ListUserState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ListUserState extends State<ListUser> {
+ 
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Future<List<Post>> fetchAllPosts() async {
+    final res = await http.get(
+        Uri.parse('https://jsonplaceholder.org/posts')); // Fixed URL
+    if (res.statusCode == 200) {
+      List jsonResponse = jsonDecode(res.body);
+      
+      var data = jsonResponse.map((post) => Post.fromJson(post)).toList();
+   
+      return data;// Print each post in the terminal
+    }else{
+      throw Exception();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      title: Text("My app"),
+       ),
+     body: Center(
+      child: FutureBuilder(future: fetchAllPosts(), builder: (context,snap){
+        if(snap.hasData){
+          return ListView.builder(
+            itemCount: snap.data?.length,
+            
+            itemBuilder: (BuildContext context, int index){
+            return Card(
+              child:  ListTile(leading:CircleAvatar(backgroundImage: NetworkImage(snap.data![index].image)),title:  Text(snap.data![index].title),subtitle:Text(snap.data![index].content,overflow: TextOverflow.ellipsis,)),
+            );
+          });
+        }
+        else if (snap.hasError){
+          return Text("no data");
+        }
+        return Center(child: CircularProgressIndicator(),);
+      }),
+     ),
+    ); 
   }
 }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//         title: Text(widget.title),
+//       ),
+      
+//       body: ListView.builder(
+//           itemCount: 10,
+//           itemBuilder: (BuildContext context, int index) {
+//             return ListTile(title: Text("List item $index"));
+//           }),
+          
+//     );
+//   }
+// }
